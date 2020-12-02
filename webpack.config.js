d68d4fceea8897638648e738b804b84c75daaf39
Sergey Polikarpov <xpalik@gmail.com>
2020-12-02 09:45:16 +0300

    7. Настойка Eslint перед занятием

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: "development",
-    entry: './index.js',
+    entry: ['@babel/polyfill','./index.js'],
     output: {
         filename: filename('js'),
         path: path.resolve(__dirname, 'dist')
@@ -30,6 +30,7 @@ module.exports = {
         hot: isDev,
         open: true, // "--open" теперь указывается здесь
     },
+    target: isDev ? 'web' : 'browserslist',
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebackPlugin({
