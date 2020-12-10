9ce6c3ff140bea60a9ddc43b97e7587578833b92
Sergey Polikarpov <xpalik@gmail.com>
2020-12-10 14:18:44 +0300

    3.6. Класс для работы с DOM

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,16 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Header extends ExcelComponent {
+    static className = 'excel__header'
     toHTML() {
-        return '<h1>Header</h1>'
+        return `            <input class="input" type="text" value="NewTab" />
+            <div>
+                <div class="button">
+                    <i class="material-icons">delete</i>
+                </div>
+                <div class="button">
+                    <i class="material-icons">exit_to_app</i>
+                </div>
+            </div>`
     }
 }
---
+++ src/core/dom.js
@@ -0,0 +1,45 @@
+class Dom {
+    constructor(selector) {
+        // #app
+        this.$el = typeof selector === 'string'
+            ? document.querySelector(selector)
+            : selector
+    }
+
+    html(html) {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+            return this
+        }
+        return this.$el.outerHTML.trim()
+    }
+    clear() {
+        this.html('')
+        return this
+    }
+    append(node) {
+        if (node instanceof Dom) {
+            node = node.$el
+        }
+        if (Element.prototype.append) {
+            this.$el.append(node)
+        } else {
+            this.$el.appendChild(node)
+        }
+        return this
+    }
+}
+
+
+// event.target
+export function $(selector) {
+    return new Dom(selector)
+}
+
+$.create = (tagName, classess = '') => {
+    const el = document.createElement(tagName)
+    if (classess) {
+        el.classList.add(classess)
+    }
+    return $(el)
+}
