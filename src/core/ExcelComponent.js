e86ad7bfb477bed4f0187f99f9cf1b9461bedf39
Sergey Polikarpov <xpalik@gmail.com>
2020-12-10 17:19:54 +0300

    3.7. Подготовка структуры для событий

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,18 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+    }
+
     // Возвращает шаблон компонента
     toHTML() {
         return ''
+    }
+    onInput() {
+        console.log('Formula: onInput', event)
+    }
+    init() {
+        this.initDOMListeners()
     }
 }
