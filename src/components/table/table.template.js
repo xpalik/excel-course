afd37f3467aedc28a3dcc0c2f1dce986b7103b78
Sergey Polikarpov <xpalik@gmail.com>
2020-12-21 15:42:59 +0300

    finish base framework functional

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,23 +3,18 @@ const CODES = {
     Z: 90
 }
 
-function createCell() {
-    return `                    
-                    <div class="cell selected" contenteditable>
-                        a1
-                    </div>
-    `
+function toCell(cell = '') {
+    return `<div class="cell" contenteditable>${cell}</div>`
 }
 
 function toColumn(col) {
-    return `<div class="column">${col}</div>
-`
+    return `<div class="column">${col}</div>`
 }
 
-function createRow(content) {
+function createRow(content, rowInfo = '') {
     return `
     <div class="row">
-        <div class="row-info"></div>
+        <div class="row-info">${rowInfo}</div>
         <div class="row-data">${content}</div>
     </div>
     `
@@ -29,7 +24,7 @@ function toChar(_, index) {
     return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 5) {
+export function createTable(rowsCount = 10) {
     const colsCount = CODES.Z - CODES.A + 1
     const rows = []
 
@@ -41,10 +36,11 @@ export function createTable(rowsCount = 5) {
 
     rows.push(createRow(cols))
     for (let i = 0; i < rowsCount; i++) {
-        rows.push(createRow())
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+        rows.push(createRow(cells, i + 1))
     }
-
-
-
     return rows.join('')
 }
